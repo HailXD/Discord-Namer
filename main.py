aa86d3311b13bc47674641dd1e6ef4093db0af92
Hail <jyealc@gmail.com>
2025-06-25 13:58:32 +0800

    Docker

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import discord
 
 if os.path.exists('.env'):
@@ -8,24 +9,31 @@ if os.path.exists('.env'):
 token = os.getenv('TOKEN')
 
 class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.last_update_time = 0
+
     async def on_ready(self):
         print('Logged on as', self.user)
 
     async def on_message(self, message):
         if isinstance(message.channel, discord.DMChannel) and message.author != self.user:
-            
+            current_time = time.time()
+            if current_time - self.last_update_time < 180: 
+                return
+
             if len(message.content) > 32:
                 parts = message.content.split()
                 new_name = ''
                 for part in parts:
                     if len(new_name) + len(part) + 1 <= 32:
                         new_name += f"{part} "
-
                 new_name = new_name.strip()
             else:
                 new_name = message.content
-
+            
             await self.user.edit(global_name=new_name)
+            self.last_update_time = current_time
 
 client = MyClient()
 client.run(token)
